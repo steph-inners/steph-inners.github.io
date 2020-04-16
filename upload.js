9d2ab7a6120a3f6c47bdbacbe496c34e09ad84f3
SDI <sdi.46m@gmail.com>
2020-04-15 20:49:50 -0400

    php

--- upload.js
+++ upload.js
@@ -1,19 +1,22 @@
-// Define processing URL and form element
-
-const url = 'process.php';
-const form = document.querySelector('form');
-
-// Listen for form submit
+const url = 'process.php'
+const form = document.querySelector('form')
 
 form.addEventListener('submit', e => {
-    e.preventDefault();
-    
-    // Gather files and begin formdata
+  e.preventDefault()
 
-    const files = document.querySelector('[type=file]').files;
-    const formData = new FormData();
+  const files = document.querySelector('[type=file]').files
+  const formData = new FormData()
+
+  for (let i = 0; i < files.length; i++) {
+    let file = files[i]
+
+    formData.append('files[]', file)
+  }
+
+  fetch(url, {
+    method: 'POST',
+    body: formData,
+  }).then(response => {
+    console.log(response)
+  })
 })
-
-
-
-
